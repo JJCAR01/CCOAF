0bdaa21cfd8bf50a1330fb39a919ae50c923b7ea
Juan José Cardona Gómez <juanjo-cardona@hotmail.com>
2023-11-24 16:03:02 -0500

    dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Chart,registerables} from 'node_modules/chart.js'
+import { PatService } from '../pat/services/pat.service';
+import { AuthService } from '../login/auth/auth.service';
+import { ActividadService } from '../actividad/services/actividad.service';
+import { TipoGEService } from '../gestion/services/tipoGE.service';
 Chart.register(...registerables)
 
 @Component({
@@ -12,20 +16,72 @@ Chart.register(...registerables)
 })
 export class DashboardComponent implements OnInit {
   title = 'dashboard';
+  data : any;
+  nombrePats : any [] = [];
+  porcentajePats : any [] = [];
+  label : any [] = [];
+  avanceProyectos : any [] = [];
+  sumaTotal : any [] = [];
+  total : any [] = [];
+
+  constructor(
+    private patService:PatService,
+    private auth:AuthService,
+    private actividadService:ActividadService,
+    private tipoService:TipoGEService){ }
   
   ngOnInit(): void {
-    this.renderChart();
+    this.patService.listarPat(this.auth.obtenerHeader()).subscribe(result => {
+      this.data = result;
+      if(this.data != null){
+        for (let i = 0; i < this.data.length; i++) {
+          this.nombrePats.push(this.data[i].nombre);
+          this.porcentajePats.push(this.data[i].porcentaje);
+        }
+        this.renderChart(this.nombrePats,this.porcentajePats,'bar','chart','Total');
+      }
+    })
+    this.actividadService.listarProyecto(this.auth.obtenerHeader()).subscribe(result => {
+      this.data = result;
+      if(this.data != null){
+        this.label.push('Proyectos');
+      }
+      this.sumaTotal.push(this.data.length);
+      
+    })
+    this.tipoService.listarGestion(this.auth.obtenerHeader()).subscribe(result => {
+      this.data = result;
+      if(this.data != null){
+        this.label.push('Actividades gestión');
+      }
+      this.sumaTotal.push(this.data.length);
+      
+    })
+    this.tipoService.listarActividadEstrategica(this.auth.obtenerHeader()).subscribe(result => {
+      this.data = result;
+      if(this.data != null){
+        this.label.push("Actividades Estratégicas");        
+      }
+      this.sumaTotal.push(this.data.length);
+      this.renderChart(this.label,this.sumaTotal,'doughnut','chart2','Total');
+    })
+
+    
   }
 
-  renderChart(){
-    const dashboard = new Chart('chart', {
-      type: 'bar',
+  renderChart(data:any, mindata:any, type:any , id:any, label:any){
+    const dashboard = new Chart(id, {
+      type: type,
       data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: data,
         datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          borderWidth: 1
+          label: label,
+          data: mindata,
+          backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)'
+          ],
         }]
       },
       options: {
@@ -36,7 +92,6 @@ export class DashboardComponent implements OnInit {
         }
       }
     });
-
   }
 
 }
